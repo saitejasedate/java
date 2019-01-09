4e2f46f53b6eee5fda5478192fd6fc3b45321f1e
saitejasedate <saitejavelagandula@msitprogram.net>
2019-01-09 16:20:39 +0530

    "ads1_m6_a1: 6/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignment-1/Assignment-1/Solution.java
+++ Assignment-1/Assignment-1/Solution.java
@@ -24,21 +24,21 @@ class AddLargeNumbers {
     }
 
     public static LinkedList addLargeNumbers(LinkedList list1, LinkedList list2) {
-        Stack<Integer> s1 = new Stack<Integer>();
-        Stack<Integer> s2 = new Stack<Integer>();
-        LinkedList ll = new LinkedList();
-        Node temp1 = list1.getHead();
-        Node temp2 = list2.getHead();
-        while (temp1.getNext() != null && temp2.getNext() != null) {
-            s1.push(temp1.getData());
-            s2.push(temp2.getData());
-            temp1 = temp1.getNext();
-            temp2 = temp2.getNext();
-        }
-        while (s1.isEmpty() && s2.isEmpty()) {
-            ll.pushRight(s1.pop()+s2.pop());
-        }
-        return ll;
+        // Stack<Integer> s1 = new Stack<Integer>();
+        // Stack<Integer> s2 = new Stack<Integer>();
+        // LinkedList ll = new LinkedList();
+        // Node temp1 = list1.getHead();
+        // Node temp2 = list2.getHead();
+        // while (temp1.getNext() != null && temp2.getNext() != null) {
+        //     s1.push(temp1.getData());
+        //     s2.push(temp2.getData());
+        //     temp1 = temp1.getNext();
+        //     temp2 = temp2.getNext();
+        // }
+        // while (s1.isEmpty() && s2.isEmpty()) {
+        //     ll.pushRight(s1.pop()+s2.pop());
+        // }
+        return null;
     }
 }
 
